141825c68a2d175722ed3ecc55c65931f5354488
Jeanlee <jeanlee.barreto@utec.edu.pe>
2022-02-02 20:13:15 -0500

    Add api route for users

--- pages/about.js
+++
@@ -1,9 +0,0 @@
-import Header from '../components/header';
-
-export default () => (
-    <>
-        <Header />
-
-        <h1>About</h1>
-    </>
-);
--- pages/contact.js
+++
@@ -1,9 +0,0 @@
-import Header from '../components/header';
-
-export default () => (
-    <>
-        <Header />
-
-        <h1>Contact</h1>
-    </>
-);
